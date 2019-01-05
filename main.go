e7b5da8a81a235448e3fd3bdafa72069980f1a33
Chris Mepham <chris.mepham@cognitran.com>
2019-01-05 17:07:38 +0000

    Added links.

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 	"text/template"
 
 	"github.com/gorilla/mux"
@@ -11,23 +14,62 @@ import (
 
 const meJSONURI string = "https://raw.githubusercontent.com/crmepham/public/master/me.json"
 const jsonFETCHERROR string = "Could not fetch JSON!"
+const staticCONTENTPATH string = "/static"
 
 // Person encapsulates my public data for use in the template.
 type Person struct {
-	FirstName   string `json:"firstName"`
-	LastName    string `json:"lastName"`
-	DateOfBirth string `json:"dateOfBirth"`
+	FirstName      string `json:"firstName"`
+	LastName       string `json:"lastName"`
+	JobTitle       string `json:"jobTitle"`
+	DateOfBirth    string `json:"dateOfBirth"`
+	PersonalEmail  string `json:"personalEmail"`
+	PersonalMobile string `json:"personalMobile"`
+	ShortBiography string `json:"shortBiography"`
+	Links          []Link `json:"links"`
+}
+
+// Link encapsulates links to my various online profiles
+type Link struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	URI         string `json:"uri"`
 }
 
 var person = &Person{}
 
+// The main method.
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", rootHandler)
+	r.HandleFunc(staticCONTENTPATH+"/{a-z}/{a-z}", staticContentHandler)
 	loadPerson()
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
 
+// The request handlers.
+func staticContentHandler(response http.ResponseWriter, request *http.Request) {
+	setContentType(response, request)
+	path := request.URL.Path
+	data, err := ioutil.ReadFile(path[1:len(path)])
+	check(err)
+	response.Write([]byte(data))
+}
+
+func rootHandler(response http.ResponseWriter, request *http.Request) {
+	setContentType(response, request)
+	temp, err := template.ParseFiles("templates/profile.html")
+	check(err)
+	temp.Execute(response, person)
+}
+
+func jsonHandler(response http.ResponseWriter, request *http.Request) {
+	setContentType(response, request)
+	unmarshalled, err := json.Marshal(&person)
+	check(err)
+	response.Write([]byte(string(unmarshalled)))
+}
+
+// The utility methods.
 func loadPerson() {
 	resp, err := http.Get(meJSONURI)
 	if err != nil || resp.StatusCode != 200 {
@@ -36,27 +78,30 @@ func loadPerson() {
 
 	defer resp.Body.Close()
 	json.NewDecoder(resp.Body).Decode(person)
+	fmt.Println(person.ShortBiography)
 }
 
-func rootHandler(response http.ResponseWriter, request *http.Request) {
-	response.Header().Set("Content-Type", "text/html")
-
-	temp, err := template.ParseFiles("templates/profile.html")
-	if err != nil {
-		response.Write([]byte("Could not load template!"))
-		return
+func setContentType(response http.ResponseWriter, request *http.Request) {
+	path := request.URL.Path
+	var suffix = string(path[strings.LastIndex(path, ".")+1 : len(path)])
+	var contentType string
+	switch suffix {
+	case "css":
+		contentType = "text/css"
+	case "js":
+		contentType = "text/javascript"
+	case "json":
+		contentType = "application/json"
+	case "jpg":
+		contentType = "image/jpg"
+	default:
+		contentType = "text/html"
 	}
-
-	temp.Execute(response, person)
+	response.Header().Set("Content-Type", contentType+"; charset=utf-8")
 }
 
-func jsonHandler(response http.ResponseWriter, request *http.Request) {
-	response.Header().Set("Content-Type", "application/json")
-
-	unmarshalled, err := json.Marshal(&person)
-	if err != nil {
-		panic(jsonFETCHERROR)
+func check(e error) {
+	if e != nil {
+		panic(e)
 	}
-
-	response.Write([]byte(string(unmarshalled)))
 }
